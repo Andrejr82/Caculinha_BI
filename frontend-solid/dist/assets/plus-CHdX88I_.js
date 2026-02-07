0d705fd6195a35a6169277edd9f6e19f10ba38db
André Mauricio <79088224+devAndrejr@users.noreply.github.com>
2026-02-07 15:39:56 -0300

    backup before history cleanup

---
+++ frontend-solid/dist/assets/plus-CHdX88I_.js
@@ -0,0 +1,1 @@
+import{c as o,m as s,I as a}from"./index-BTwf99OE.js";const t=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],c=e=>o(a,s(e,{name:"Plus",iconNode:t}));export{c as P};
